e7a66f4cb74b6c87bef91e7bc082b49f67a9b52a
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-02-04 21:01:29 -0600

    add entriesController, and test the api

---
+++ BugetBookSPA/Controllers/EntriesController.cs
@@ -0,0 +1,30 @@
+﻿using BugetBookSPA.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BugetBookSPA.Controllers
+{
+    public class EntriesController : ApiController
+    {
+        public IHttpActionResult GetEntries()
+        {
+            try
+            {
+                //"Uing" Open connection once everything below in the bracket is done.
+                using (var context = new AppDbContext())
+                {
+                    var entries = context.Entries.ToList();
+                    return Ok(entries);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
