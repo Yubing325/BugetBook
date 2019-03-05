60b12a6f4bd625e178773df9076ca44c10b41444
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-03-05 06:23:10 -0600

    Upload client side

---
+++ Client/src/app/entry.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EntryService {
+
+  baseUrl: string = 'http://localhost:7634/api/entries/'
+
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get(this.baseUrl);
+  }
+
+  createEntry(entry) {
+    return this.http.post(this.baseUrl, entry);
+  }
+}
+
---
+++ Client/src/app/interfaces/EntryElement.ts
@@ -0,0 +1,5 @@
+export interface EntryElement {
+    Descirption: string;
+    IsExpense: boolean;
+    Value: number;
+}
